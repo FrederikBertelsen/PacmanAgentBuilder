8e7c2e87ae212e1d5320e4a9d23884a3541cbc62
Frederik Bertelsen <frbb@itu.dk>
2024-03-06 16:50:10 +0100

    actually fix path for linux

--- Pacman_Complete/sprites.py
+++ Pacman_Complete/sprites.py
@@ -12,7 +12,7 @@ DEATH = 5
 
 class Spritesheet(object):
     def __init__(self):
-        self.sheet = pygame.image.load(os.path.join("Pacman_complete", "spritesheet_mspacman.png")).convert()
+        self.sheet = pygame.image.load(os.path.join("Pacman_Complete", "spritesheet_mspacman.png")).convert()
         transcolor = self.sheet.get_at((0, 0))
         self.sheet.set_colorkey(transcolor)
         width = int(self.sheet.get_width() / BASETILEWIDTH * TILEWIDTH)
